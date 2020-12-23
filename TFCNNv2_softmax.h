5417418c9caf48bfd5906ccbbecb2ce1465c484c
TFCNN <73448936+TFCNN@users.noreply.github.com>
2020-12-23 14:48:07 +0000

    Update TFCNNv2_softmax.h

--- TFCNNv2_softmax.h
+++ TFCNNv2_softmax.h
@@ -243,6 +243,7 @@ int loadNetwork(network* net, const char* file);
 --------------------------------------
 */
 
+
 float qRandFloat(const float min, const float max)
 {
 #ifndef FAST_PREDICTABLE_MODE
@@ -253,7 +254,7 @@ float qRandFloat(const float min, const float max)
         ls = time(0) + 33;
     }
 #endif
-    return ( ((float)rand() / (float)RAND_MAX) * (max-min) ) + min;
+    return ( ( ((float)rand())+1e-7 / (float)RAND_MAX ) * (max-min) ) + min;
 }
 
 float uRandFloat(const float min, const float max)
@@ -266,7 +267,7 @@ float uRandFloat(const float min, const float max)
     ssize_t result = read(f, &s, 4);
     srand(s);
     close(f);
-    return ( ((float)rand() / (float)RAND_MAX) * (max-min) ) + min;
+    return ( ( ((float)rand())+1e-7 / (float)RAND_MAX ) * (max-min) ) + min;
 #endif
 }
 
@@ -283,7 +284,7 @@ float qRandWeight(const float min, const float max)
     float pr = 0;
     while(pr == 0) //never return 0
     {
-        const float rv2 = ( ((float)rand() / (float)RAND_MAX) * (max-min) ) + min;
+        const float rv2 = ( ( ((float)rand())+1e-7 / (float)RAND_MAX ) * (max-min) ) + min;
         pr = roundf(rv2 * 100) / 100; // two decimals of precision
     }
     return pr;
@@ -302,7 +303,7 @@ float uRandWeight(const float min, const float max)
     float pr = 0;
     while(pr == 0) //never return 0
     {
-        const float rv2 = ( ((float)rand() / (float)RAND_MAX) * (max-min) ) + min;
+        const float rv2 = ( ( ((float)rand())+1e-7 / (float)RAND_MAX ) * (max-min) ) + min;
         pr = roundf(rv2 * 100) / 100; // two decimals of precision
     }
     return pr;
@@ -320,7 +321,7 @@ uint qRand(const uint min, const uint umax)
     }
 #endif
     const uint max = umax + 1;
-    return ( ((float)rand() / (float)RAND_MAX) * (max-min) ) + min;
+    return ( ( ((float)rand())+1e-7 / (float)RAND_MAX ) * (max-min) ) + min;
 }
 
 uint uRand(const uint min, const uint umax)
@@ -334,7 +335,7 @@ uint uRand(const uint min, const uint umax)
     srand(s);
     close(f);
     const uint max = umax + 1;
-    return ( ((float)rand() / (float)RAND_MAX) * (max-min) ) + min;
+    return ( ( ((float)rand())+1e-7 / (float)RAND_MAX ) * (max-min) ) + min;
 #endif
 }
 
@@ -348,13 +349,13 @@ float qRandNormal() // Box Muller
         ls = time(0) + 33;
     }
 #endif
-    double u = ((float)rand() / (float)RAND_MAX) * 2 - 1;
-    double v = ((float)rand() / (float)RAND_MAX) * 2 - 1;
+    double u = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
+    double v = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
     double r = u * u + v * v;
     while(r == 0 || r > 1)
     {
-        u = ((float)rand() / (float)RAND_MAX) * 2 - 1;
-        v = ((float)rand() / (float)RAND_MAX) * 2 - 1;
+        u = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
+        v = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
         r = u * u + v * v;
     }
     return u * sqrt(-2 * log(r) / r);
@@ -370,13 +371,13 @@ float uRandNormal()
     ssize_t result = read(f, &s, 4);
     srand(s);
     close(f);
-    double u = ((float)rand() / (float)RAND_MAX) * 2 - 1;
-    double v = ((float)rand() / (float)RAND_MAX) * 2 - 1;
+    double u = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
+    double v = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
     double r = u * u + v * v;
     while(r == 0 || r > 1)
     {
-        u = ((float)rand() / (float)RAND_MAX) * 2 - 1;
-        v = ((float)rand() / (float)RAND_MAX) * 2 - 1;
+        u = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
+        v = ( ((float)rand())+1e-7 / (float)RAND_MAX) * 2 - 1;
         r = u * u + v * v;
     }
     return u * sqrt(-2 * log(r) / r);
